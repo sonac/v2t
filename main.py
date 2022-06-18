2ab049e172c00c1e967e3a192a6adbd14510f40d
sonac <sonag007@gmail.com>
2022-06-18 15:33:14 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+from trascrypt import Transcryptor
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+class Bot:
+  def __init__(self):
+    self.updater = None
+    self.dispatcher = None
+    self.handlers = []
+    load_dotenv()
+    token = os.getenv('TELEGRAM_TOKEN')
+    self.app = ApplicationBuilder().token(token).build()
+    self.transcryptor = Transcryptor()
+
+  async def get_voice(self, update: Update, context) -> None:
+    # get basic info about the voice note file and prepare it for downloading
+    new_file = await context.bot.get_file(update.message.voice.file_id)
+    # download the voice note as a file
+    await new_file.download(f"voice_note.ogg")
+    # transcrypt the voice note
+    transcrypted_text = self.transcryptor.transcrypt("voice_note.ogg")
+    # send the transcrypted text to the user
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=transcrypted_text)
+
+  async def get_video(self, update: Update, context) -> None:
+    # get basic info about the voice note file and prepare it for downloading
+    new_file = await context.bot.get_file(update.message.video_note.file_id)
+    # download the voice note as a file
+    await new_file.download(f"video_note.mp4")
+    # transcrypt the voice note
+    transcrypted_text = self.transcryptor.transcrypt("video_note.mp4")
+    # send the transcrypted text to the user
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=transcrypted_text)
+
+  def start(self):
+    self.app.add_handler(MessageHandler(filters.VOICE, self.get_voice))
+    self.app.add_handler(MessageHandler(filters.VIDEO_NOTE, self.get_video))
+    self.app.run_polling()
+
+
+if __name__ == '__main__':
+  bot = Bot()
+  bot.start()
---
+++ trascrypt.py
@@ -0,0 +1,38 @@
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import subprocess
+import json 
+
+class Transcryptor:
+  def __init__(self) -> None:
+    self.sample_rate=16000
+    self.model = Model("vosk-model-ru-0.22")
+    self.rec = KaldiRecognizer(self.model, self.sample_rate)
+    self.cur_res = ""
+
+  def append(self, res) -> None:
+    js = json.loads(res)
+    if 'text' in js.keys():
+      self.cur_res += js['text']
+    if 'partial' in js.keys():
+      self.cur_res += js['partial']
+    self.cur_res += '.\n'
+    
+
+  def transcrypt(self, file_path) -> str:
+    self.cur_res = ""
+    process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i', file_path, '-ar', str(self.sample_rate),
+      '-ac', '1', '-f', 's16le', '-'], stdout=subprocess.PIPE)
+    while True:
+      data = process.stdout.read(4000)
+
+      if len(data) == 0:
+        break
+      if self.rec.AcceptWaveform(data):
+        self.append(self.rec.Result())
+      else:
+        print(self.rec.PartialResult())
+    
+    self.append(self.rec.FinalResult())
+
+    self.cur_res = self.cur_res[:-2]
+    return self.cur_res
